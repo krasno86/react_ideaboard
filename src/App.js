8d9dc0020ec2a10705a41812519a71dc4bd1a0d2
krasno86 <krasno86@mail.ru>
2018-07-11 11:21:20 +0300

    add react hash router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import './style.css'
 import Hello from './Hello'
 import ContactsList from './ContactsList'
 import IdeasContainer from './IdeasContainer'
+import { HashRouter, Route, Link, NavLink } from 'react-router-dom'
 
 class App extends Component {
     render() {
         return (
-            <div className="root">
-                {/*<Hello />*/}
-                {/*<ContactsList />*/}
-                <IdeasContainer />
-            </div>
+            <HashRouter>
+                <div>
+                    <div>
+                        <h2>Main menu</h2>
+                        <div><NavLink activeStyle = {{color: 'red'}} to='/home'>HOME</NavLink></div>
+                        <div><NavLink activeStyle = {{color: 'red'}} to='/hello'>hello</NavLink></div>
+                        <div><NavLink activeStyle = {{color: 'red'}} to='/contacts'>contacts</NavLink></div>
+                        <div><NavLink activeStyle = {{color: 'red'}} to='/ideas'>ideas</NavLink></div>
+                    </div>
+                    <div className="root">
+                        <Route path = '/hello' component = {Hello}/>
+                        <Route path = '/contacts' component = {ContactsList}/>
+                        <Route path = '/ideas' component = {IdeasContainer}/>
+                        <Route path = '/#' component = {App}/>
+                        {/*<IdeasContainer />*/}
+                    </div>
+                </div>
+            </HashRouter>
         );
     }
 }
