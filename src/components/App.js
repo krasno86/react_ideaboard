395d647d7d1f1f9016c11ca71a350cc37c55d7e5
krasno86 <krasno86@mail.ru>
2018-08-13 16:21:45 +0300

    add counter component using redux

--- src/components/App.js
+++ src/components/App.js
@@ -4,36 +4,43 @@ import Hello from './Hello'
 import ContactsList from './ContactsList'
 import Registration from './Registration'
 import Login from './Login'
+import Counter from './Counter'
 import ReactDatePicker from './ReactDatePicker'
 import IdeasContainer from './IdeasContainer'
 import { BrowserRouter, Route, NavLink } from 'react-router-dom'
+import store from '../store'
+import {Provider} from "react-redux"
 
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <div>
+            <Provider store = {store}>
+                <BrowserRouter>
                     <div>
-                        <h2>Main menu</h2>
-                        <div><NavLink activeStyle = {{color: 'red'}} to='/home'>HOME</NavLink></div>
-                        <div><NavLink activeStyle = {{color: 'red'}} to='/datepicker'>datepicker</NavLink></div>
-                        <div><NavLink activeStyle = {{color: 'red'}} to='/registration'>registration</NavLink></div>
-                        <div><NavLink activeStyle = {{color: 'red'}} to='/login'>log in</NavLink></div>
-                        <div><NavLink activeStyle = {{color: 'red'}} to='/hello'>hello</NavLink></div>
-                        <div><NavLink activeStyle = {{color: 'red'}} to='/contacts'>contacts</NavLink></div>
-                        <div><NavLink activeStyle = {{color: 'red'}} to='/ideas'>ideas</NavLink></div>
+                        <div>
+                            <h2>Main menu</h2>
+                            <div><NavLink activeStyle = {{color: 'red'}} to='/home'>HOME</NavLink></div>
+                            <div><NavLink activeStyle = {{color: 'red'}} to='/datepicker'>datepicker</NavLink></div>
+                            <div><NavLink activeStyle = {{color: 'red'}} to='/registration'>registration</NavLink></div>
+                            <div><NavLink activeStyle = {{color: 'red'}} to='/login'>log in</NavLink></div>
+                            <div><NavLink activeStyle = {{color: 'red'}} to='/hello'>hello</NavLink></div>
+                            <div><NavLink activeStyle = {{color: 'red'}} to='/counter'>Counter</NavLink></div>
+                            <div><NavLink activeStyle = {{color: 'red'}} to='/contacts'>contacts</NavLink></div>
+                            <div><NavLink activeStyle = {{color: 'red'}} to='/ideas'>ideas</NavLink></div>
+                        </div>
+                        <div className="root">
+                            <Route path = '/hello' component = {Hello}/>
+                            <Route path = '/datepicker' component = {ReactDatePicker}/>
+                            <Route path = '/contacts' component = {ContactsList}/>
+                            <Route path = '/ideas' component = {IdeasContainer}/>
+                            <Route path = '/counter' component = {Counter}/>
+                            <Route path = '/registration' component = {Registration}/>
+                            <Route path = '/login' component = {Login}/>
+                            <Route path = '/#' component = {App}/>
+                        </div>
                     </div>
-                    <div className="root">
-                        <Route path = '/hello' component = {Hello}/>
-                        <Route path = '/datepicker' component = {ReactDatePicker}/>
-                        <Route path = '/contacts' component = {ContactsList}/>
-                        <Route path = '/ideas' component = {IdeasContainer}/>
-                        <Route path = '/registration' component = {Registration}/>
-                        <Route path = '/login' component = {Login}/>
-                        <Route path = '/#' component = {App}/>
-                    </div>
-                </div>
-            </BrowserRouter>
+                </BrowserRouter>
+            </Provider>
         );
     }
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {increment} from '../AC'
+
+class Counter extends React.Component {
+    static propTypes = {
+        counter: PropTypes.number
+    }
+
+    render() {
+        return (
+            <div className='Hello'>
+                <h2>{this.props.counter}</h2>
+                <button onClick = {this.handleIncrement}>Increment counter</button>
+            </div>
+        )
+    }
+    handleIncrement = () => {
+        this.props.increment()
+    }
+}
+
+export default connect((state) => ({
+    counter: state.count
+}), {increment})(Counter)
