0363d4d7966828abc51cb16fb3eb81aa4f919bf2
krasno86 <krasno86@mail.ru>
2018-09-07 15:13:02 +0300

    add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import './ideas.css'
-import './app.css'
 import Hello from './Hello'
 import ContactsList from './ContactsList'
 import Registration from './Registration'
@@ -12,6 +10,8 @@ import IdeasContainer from './IdeasContainer'
 import { BrowserRouter, Route, NavLink } from 'react-router-dom'
 import store from '../store'
 import {Provider} from "react-redux"
+import './ideas.css'
+import './app.css'
 
 class App extends Component {
     render() {
@@ -21,17 +21,28 @@ class App extends Component {
                     <div>
                         <div className="header">
                             <div className="container">
-                                <nav className="navbar navbar-light">
-                                    <h2>Main menu</h2>
-                                    {/*<div><NavLink activeStyle = {{color: 'red'}} to='/home'>HOME</NavLink></div>*/}
-                                    <div><NavLink activeStyle = {{color: 'red'}} to='/datepicker'>datepicker</NavLink></div>
-                                    <div><NavLink activeStyle = {{color: 'red'}} to='/registration'>registration</NavLink></div>
-                                    <div><NavLink activeStyle = {{color: 'red'}} to='/login'>log in</NavLink></div>
-                                    <div><NavLink activeStyle = {{color: 'red'}} to='/hello'>hello</NavLink></div>
-                                    <div><NavLink activeStyle = {{color: 'red'}} to='/articles'>article list</NavLink></div>
-                                    <div><NavLink activeStyle = {{color: 'red'}} to='/counter'>Counter</NavLink></div>
-                                    <div><NavLink activeStyle = {{color: 'red'}} to='/contacts'>contacts</NavLink></div>
-                                    <div><NavLink activeStyle = {{color: 'red'}} to='/ideas'>ideas</NavLink></div>
+                                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                                    {/*<li><NavLink activeStyle = {{color: 'red'}} to='/home'>HOME</NavLink></li>*/}
+                                    <a className="navbar-brand" href="#">Navbar</a>
+                                    <button className="navbar-toggler" type="button" data-toggle="collapse" 
+                                                                    data-target="#navbarNavAltMarkup" 
+                                                                    aria-controls="navbarNavAltMarkup"
+                                                                    aria-expanded="false" 
+                                                                    aria-label="Toggle navigation">
+                                        <span className="navbar-toggler-icon"></span>
+                                    </button>
+                                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                        <div className="navbar-nav">
+                                            <NavLink className="nav-item nav-link" activeStyle = {{color: 'red'}} to='/datepicker'>datepicker</NavLink>
+                                            <NavLink className="nav-item nav-link" activeStyle = {{color: 'red'}} to='/registration'>registration</NavLink>
+                                            <NavLink className="nav-item nav-link" activeStyle = {{color: 'red'}} to='/login'>log in</NavLink>
+                                            <NavLink className="nav-item nav-link" activeStyle = {{color: 'red'}} to='/hello'>hello</NavLink>
+                                            <NavLink className="nav-item nav-link" activeStyle = {{color: 'red'}} to='/articles'>article list</NavLink>
+                                            <NavLink className="nav-item nav-link" activeStyle = {{color: 'red'}} to='/counter'>Counter</NavLink>
+                                            <NavLink className="nav-item nav-link" activeStyle = {{color: 'red'}} to='/contacts'>contacts</NavLink>
+                                            <NavLink className="nav-item nav-link" activeStyle = {{color: 'red'}} to='/ideas'>ideas</NavLink>
+                                        </div>
+                                    </div>
                                 </nav>
                             </div>
                         </div>
